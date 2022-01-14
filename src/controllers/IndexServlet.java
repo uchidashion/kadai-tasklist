0b112e4730b3989fdda7e7e5a2683e15050367ce
uchidashien <uchidashien@localhost>
2022-01-14 21:37:14 +0900

    IndexServletの修正 index.jspの作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,10 +33,24 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //EntityManagerのインスタンス生成
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Tasks>tasks = em.createNamedQuery("getAllTasks", Tasks.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+        //getAllTasksのSQL文で問い合わせをし、その結果をリストに格納
+        List<Tasks> tasks = em.createNamedQuery("getAllTasks", Tasks.class).getResultList();
+
+        //DBの接続切断
+        em.close();
+
+        //問い合わせた結果をリクエストスコープに保存
+        request.setAttribute("tasks", tasks);
+
+        //index.jspを呼び出す
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
+
+
+
 
     }
 
